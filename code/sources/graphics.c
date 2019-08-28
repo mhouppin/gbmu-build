f11c0e176cca85a4957cd88739e7c29fab8c1601
Morgan Houppin <mhouppin@z2r6p4.le-101.fr>
2019-08-28 16:56:20 +0200

    Suppressed Makefile (needs to be rebuilded every time), now works correctly on MacOS

--- code/sources/graphics.c
+++ code/sources/graphics.c
@@ -6,7 +6,7 @@
 /*   By: mhouppin <mhouppin@le-101.fr>              +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/07/05 01:48:43 by mhouppin     #+#   ##    ##    #+#       */
-/*   Updated: 2019/08/08 14:16:47 by mhouppin    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/08/28 16:45:31 by mhouppin    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -29,16 +29,6 @@ int32_t			*g_dpixels;
 
 void	gr_init_window(void)
 {
-	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_EVENTS) != 0)
-	{
-		fprintf(stderr, "SDL_Init() failed: %s", SDL_GetError());
-		exit(EXIT_FAILURE);
-	}
-
-	g_window = SDL_CreateWindow(g_cart.game_title,
-		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 160, 144,
-		SDL_WINDOW_RESIZABLE);
-
 	g_dwindow = SDL_CreateWindow("RAM dump",
 		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 256, 32 + 32 * g_cart.n_ram_banks,
 		SDL_WINDOW_RESIZABLE);
@@ -48,7 +38,6 @@ void	gr_init_window(void)
 		exit(EXIT_FAILURE);
 	}
 
-	g_render = SDL_CreateRenderer(g_window, -1, 0);
 	g_drender = SDL_CreateRenderer(g_dwindow, -1, 0);
 	if (g_render == NULL || g_drender == NULL)
 	{
@@ -56,11 +45,8 @@ void	gr_init_window(void)
 		exit(EXIT_FAILURE);
 	}
 
-	SDL_SetRenderDrawColor(g_render, 0, 0, 0, 255);
 	SDL_SetRenderDrawColor(g_drender, 0, 0, 0, 255);
 
-	g_texture = SDL_CreateTexture(g_render, SDL_PIXELFORMAT_ARGB8888,
-			SDL_TEXTUREACCESS_STREAMING, 160, 144);
 	g_dtexture = SDL_CreateTexture(g_drender, SDL_PIXELFORMAT_ARGB8888,
 			SDL_TEXTUREACCESS_STREAMING, 256, 32 + 32 * g_cart.n_ram_banks);
 	if (g_texture == NULL || g_dtexture == NULL)
@@ -69,10 +55,8 @@ void	gr_init_window(void)
 		exit(EXIT_FAILURE);
 	}
 
-	SDL_RenderSetLogicalSize(g_render, 160, 144);
 	SDL_RenderSetLogicalSize(g_drender, 256, 32 + 32 * g_cart.n_ram_banks);
 
-	SDL_LockTexture(g_texture, NULL, (void **)&g_pixels, &g_pitch);
 	SDL_LockTexture(g_dtexture, NULL, (void **)&g_dpixels, &g_pitch);
 
 	g_ticks = SDL_GetTicks();
--- code/sources/main.c
+++ code/sources/main.c
@@ -6,7 +6,7 @@
 /*   By: mhouppin <mhouppin@le-101.fr>              +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/07/05 01:48:55 by mhouppin     #+#   ##    ##    #+#       */
-/*   Updated: 2019/07/05 01:48:56 by mhouppin    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/08/28 16:46:49 by mhouppin    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -16,17 +16,37 @@
 #include "launcher.h"
 #include <stdio.h>
 
-int		main(int argc, char **argv)
+int		main(void)
 {
-	if (argc != 2)
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_EVENTS) != 0)
 	{
-		fprintf(stderr, "%s cartridge_file\n", *argv);
-		return (1);
+		fprintf(stderr, "SDL_Init() failed: %s", SDL_GetError());
+		exit(EXIT_FAILURE);
 	}
+	g_window = SDL_CreateWindow(g_cart.game_title,
+		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 160, 144,
+		SDL_WINDOW_RESIZABLE);
+	g_render = SDL_CreateRenderer(g_window, -1, 0);
+	SDL_SetRenderDrawColor(g_render, 0, 0, 0, 255);
+	g_texture = SDL_CreateTexture(g_render, SDL_PIXELFORMAT_ARGB8888,
+			SDL_TEXTUREACCESS_STREAMING, 160, 144);
+	SDL_RenderSetLogicalSize(g_render, 160, 144);
+	SDL_LockTexture(g_texture, NULL, (void **)&g_pixels, &g_pitch);
 
-	open_cartridge(argv[1]);
-
-	gr_init_window();
+	SDL_Event ev;
+	while (SDL_PollEvent(&ev))
+	{
+		if (ev.type == SDL_QUIT || (ev.type == SDL_WINDOWEVENT &&
+					ev.window.type == SDL_WINDOWEVENT_CLOSE))
+			exit(EXIT_SUCCESS);
+		else if (ev.type == SDL_DROPFILE)
+		{
+			open_cartridge(ev.drop.file);
+			gr_init_window();
+			SDL_free(ev.drop.file);
+			break ;
+		}
+	}
 
 	start_game();
 	return (0);
